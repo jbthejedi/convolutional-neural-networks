495d0aa0f8218e4f47c59cf9944731c61778fbea
Justin Barry <justinbarrythejedi@gmail.com>
2025-02-12 13:09:59 +0700

    finished modules for video

--- src/nn_modules.py
+++ src/nn_modules.py
@@ -6,14 +6,17 @@ from dataclasses import dataclass
 
 @dataclass
 class Config:
+    batch_size : int = 32
     weight_init_scale : float = 0.02
-    batch_size : int = 8
 
-# ioksp
 class MyConv2d(nn.Module):
+    """
+    ioksp: c_in, c_out, kern, strid, pad
+    h_out = (h_in + 2*p - k) // s + 1
+    """
     def __init__(
         self, in_channels, out_channels,
-        kernel_size, stride, padding, weight_scale
+        kernel_size, stride, padding, weight_init_scale
     ):
         super().__init__()
         self.in_channels = in_channels
@@ -21,39 +24,35 @@ class MyConv2d(nn.Module):
         self.kernel_size = kernel_size
         self.stride = stride
         self.padding = padding
-        
-        s = weight_scale
-        self.weights = nn.Parameter( # (c_out, c_in, k, k) => (c_out, c_in*k*k)
-            torch.randn(out_channels, in_channels, kernel_size, kernel_size) * s
+
+        scale = weight_init_scale
+        # weights.shape => (c_out, c_in, k, k)
+        self.weights = nn.Parameter(
+            torch.randn(out_channels, in_channels, kernel_size, kernel_size) * scale
         )
 
     def forward(self, x):
         """
-        in shape (b, c_in, h_in, w_in)
+        in.shape (b, c_in, h_in, w_in)
         return shape (b, c_out, h_out, w_out)
+        where h_out = (h_in + 2p - k) // s + 1
         """
         b, c_in, h_in, w_in = x.shape
         k, s, p = self.kernel_size, self.stride, self.padding
-
-        # unfold => (b, c_in*k*k, l), where l = h_out * w_out is
-        # the number of extracted patches.
-        x_unfold = F.unfold( # (b, l, c_in*k*k)
-                x, kernel_size=k, stride=s, padding=p
+        x_unfold = F.unfold( # (b, c_in*k*k, l), where l = h_out * w_out
+            x, kernel_size=k, stride=s, padding=p
         )
         x_unfold_t = x_unfold.transpose(1, 2) # (b, l, c_in*k*k)
         weights = self.weights.view(-1, c_in*k*k) # (c_out, c_in*k*k)
-        out = x_unfold_t @ weights.t() # (b, l, c_in*k*k) @ (c_in*k*k, c_out) => (b, l, c_out), where l = (h_out, w_out)
-        # Now, we want shape (b, c_out, h_out, w_out)
-        h_out = (h_in + 2*p - k) // s + 1
-        w_out = (w_in + 2*p - k) // s + 1
+        out = x_unfold_t @ weights.t() # (b, l, c_in*k*k) x (c_in*k*k, c_out) => (b, l, c_out)
+        h_out = (h_in + 2 * p - k ) // s + 1
+        w_out = (w_in + 2 * p - k ) // s + 1
         out = out.view(b, -1, h_out, w_out)
 
         return out
 
 class MyMaxPool2d(nn.Module):
-    def __init__(
-        self, kernel_size, stride, padding
-    ):
+    def __init__(self, kernel_size, stride, padding):
         super().__init__()
         self.kernel_size = kernel_size
         self.stride = stride
@@ -62,14 +61,15 @@ class MyMaxPool2d(nn.Module):
     def forward(self, x):
         b, c_in, h_in, w_in = x.shape
         k, s, p = self.kernel_size, self.stride, self.padding
-        x_unfold = F.unfold( # (b, c_in*k*k, h_out*w_out)
+        x_unfold = F.unfold( # (b, c_in*k*k, l), where l = h_out*w_out
             x, kernel_size=k, stride=s, padding=p
         )
-        x_unfold = x_unfold.view(b, c_in, k*k, -1) # (b, c_in, k*k, h_out*w_out)
-        x_max, _ = x_unfold.max(dim=2) # (b, c_in, h_out*w_out)
+        x_unfold = x_unfold.view(b, c_in, k*k, -1) # (b, c_in, k*k, l)
+        xmax, _ = x_unfold.max(dim=2) # (b, c_in, l)
         h_out = (h_in + 2*p - k) // s + 1
         w_out = (w_in + 2*p - k) // s + 1
-        out = x_max.view(b, c_in, h_out, w_out)
+        out = xmax.view(b, c_in, h_out, w_out)
+
         return out
 
 class MyAvgPool2d(nn.Module):
@@ -79,79 +79,66 @@ class MyAvgPool2d(nn.Module):
 
     def forward(self, x):
         b, c_in, h_in, w_in = x.shape
-
-        h_out, w_out = self.output_size[0], self.output_size[1]
+        h_out, w_out = self.output_size
         kernel_h = h_in // h_out
         kernel_w = w_in // w_out
-        kernel_size = (kernel_h, kernel_w) 
-        
-        x_unfold = F.unfold( # (b, c_in*k*k, h_out*w_out)
-            x, kernel_size=kernel_size, stride=kernel_size, # padding=0
+        kernel_size = (kernel_h, kernel_w)
+        x_unfold = F.unfold( # (b, c_in*k*k, l)
+            x, kernel_size=kernel_size, stride=kernel_size
         )
-        x_unfold = x_unfold.view(b, c_in, kernel_h * kernel_w, h_out, w_out)
-        out = x_unfold.mean(dim=2) # (b, c_in, h_out, w_out)
-
+        x_unfold = x_unfold.view(b, c_in, kernel_h * kernel_w, -1)
+        xmean = x_unfold.mean(dim=2) # (b, c_in, l)
+        out = xmean.view(b, c_in, h_out, w_out)
         return out
 
-        
-
 def test_modules(config : Config):
-    # --------------
+    # ----------
     # MyConv2d
-    # --------------
+    # ----------
     module_name = "MyConv2d"
-    b, c_in, h_in, w_in = (6, 3, 32, 32)
     module = MyConv2d(
-        in_channels=c_in,
+        in_channels=3,
         out_channels=20,
         kernel_size=3,
         stride=1,
         padding=1,
-        weight_scale=config.weight_init_scale,
+        weight_init_scale=config.weight_init_scale,
     )
-    print(f"Testing module {module_name}")
-    in_tensor = torch.zeros(b, c_in, h_in, w_in)
 
-    # in (6, 3, 32, 32) => out (6, 20, 32, 32)
+    in_tensor = torch.randn((6, 3, 32, 32))
     out_tensor = module(in_tensor)
     expected_shape = (6, 20, 32, 32)
     assert out_tensor.shape == expected_shape, f"Failed. Expected {expected_shape} but got {out_tensor.shape}"
     print(f"out_tensor.shape {out_tensor.shape}")
 
-    # --------------
+    # ----------
     # MyMaxPool2d
-    # --------------
+    # ----------
     module_name = "MyMaxPool2d"
-    b, c_in, h_in, w_in = (6, 3, 32, 32)
     module = MyMaxPool2d(
         kernel_size=3,
         stride=1,
         padding=1,
     )
-    print(f"Testing module {module_name}")
-    in_tensor = torch.zeros(b, c_in, h_in, w_in)
 
-    # in (6, 3, 32, 32) => out (6, 20, 32, 32)
+    in_tensor = torch.randn((6, 3, 32, 32))
     out_tensor = module(in_tensor)
-    expected_shape = (6, c_in, h_in, w_in)
+    expected_shape = (6, 3, 32, 32)
     assert out_tensor.shape == expected_shape, f"Failed. Expected {expected_shape} but got {out_tensor.shape}"
     print(f"out_tensor.shape {out_tensor.shape}")
 
-    # --------------
+    # ----------
     # MyAvgPool2d
-    # --------------
+    # ----------
     module_name = "MyAvgPool2d"
-    b, c_in, h_in, w_in = (6, 3, 32, 32)
-    output_size = (1,1)
-    module = MyAvgPool2d(output_size=output_size)
-    print(f"Testing module {module_name}")
-    in_tensor = torch.zeros(b, c_in, h_in, w_in)
+    module = MyAvgPool2d(output_size=(1, 1))
 
-    # in (6, 3, 32, 32) => out (6, 20, 32, 32)
+    in_tensor = torch.randn((6, 20, 8, 8)) 
     out_tensor = module(in_tensor)
-    expected_shape = (6, c_in, output_size[0], output_size[1])
+    expected_shape = (6, 20, 1, 1)
     assert out_tensor.shape == expected_shape, f"Failed. Expected {expected_shape} but got {out_tensor.shape}"
     print(f"out_tensor.shape {out_tensor.shape}")
+
 
 def main():
     config = Config()
